9822162654a6e83b0192304a624988e6b3a0ed9b
Lucas <vieiralucas1199@gmail.com>
2024-09-05 15:06:16 -0300

    finalizando o projeto

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,28 @@
 AOS.init();
+
+const dataDoEvento = new Date('Dec 11, 2024 20:00:00');
+const timeStampEvento = dataDoEvento.getTime();
+
+const contador = setInterval(function() {
+    const agora = new Date();
+    const timeStampAtual = agora.getTime();
+
+    const tempoAteEvento = timeStampEvento - timeStampAtual;
+
+    const diaEmMs = 1000 * 60 * 60 * 24;
+    const horaEmMs = 1000 * 60 * 60;
+    const minEmMs = 1000 * 60;
+
+    const dias = Math.floor(tempoAteEvento / diaEmMs);
+    const horas = Math.floor((tempoAteEvento % diaEmMs) / horaEmMs);
+    const minutos = Math.floor((tempoAteEvento % horaEmMs) / minEmMs);
+    const segundos = Math.floor((tempoAteEvento % minEmMs) / 1000);
+
+    document.getElementById('contador').innerHTML = `${dias}d ${horas}h ${minutos}m ${segundos}s`
+
+    if(tempoAteEvento < 0) {
+        clearInterval(contador);
+        document.getElementById('contador').innerHTML = 'Evento Expirado';
+    }
+
+}, 1000);
